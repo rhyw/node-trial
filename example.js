188b943bcf32688aed04f8ac4e9078eae19d8dac
Yuguang Wang <wangyg819@gmail.com>
2011-09-30 16:23:19 +0800

    ex: a simple hello, world in node

---
+++ example.js
@@ -0,0 +1,12 @@
+#!/usr/local/bin/node
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  console.log('Request Received.');
+  console.log('Begin to rename file.');
+  fs.rename('/tmp/hello', '/tmp/world');
+  console.log('Rename finishied.');
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!\n');
+}).listen(1337, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:1337/');
