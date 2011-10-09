a653de57ca598e977f9887b75188d5b75ceb2adc
Yuguang Wang <wangyg819@gmail.com>
2011-10-09 15:29:32 +0800

    update screenrc

--- example.js
+++ example.js
@@ -5,7 +5,7 @@ http.createServer(function (req, res) {
   console.log('Request Received.');
   console.log('Begin to rename file.');
   fs.rename('/tmp/hello', '/tmp/world');
-  console.log('Rename finishied.');
+  console.log('Rename finished.');
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!\n');
 }).listen(1337, "127.0.0.1");
