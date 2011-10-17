2b2972a6dbd19f43c458c7ca42eca5883d640ee0
Yuguang Wang <wangyg819@gmail.com>
2011-10-17 17:29:28 +0800

    Add tcp chat server

---
+++ node-ryan/chat.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+var net = require('net');
+
+var sockets = []
+
+var s = net.Server(function(socket){
+    sockets.push(socket);
+
+    socket.on('data', function(d){
+        for (var i = 0; i < sockets.length; i++)
+            sockets[i].write(d);
+    })
+
+    socket.on('end', function(d){
+        var i = sockets.indexOf(socket);
+        sockets.splice(i, 1);
+    })
+})
+
+s.listen(8000, '0.0.0.0');
