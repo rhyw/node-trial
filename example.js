b613ef77e0a2ecb55f86a3271b0767b2fb152b34
Yuguang Wang <wangyg819@gmail.com>
2011-10-24 17:44:05 +0800

    add jsconf 09-10 pdfs

--- example.js
+++ example.js
@@ -2,10 +2,10 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
-  //console.log('Request Received.');
-  //console.log('Begin to rename file.');
-  //fs.rename('/tmp/hello', '/tmp/world');
-  //console.log('Rename finished.');
+  // console.log('Request Received.');
+  // console.log('Begin to rename file.');
+  // fs.rename('/tmp/hello', '/tmp/world');
+  // console.log('Rename finished.');
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!\n');
 }).listen(1337, "127.0.0.1");
